f8a815d367409a2fd9389e849f554d7b3a540148
JeffreyGallegosCoding <jeffreygallegos01@gmail.com>
2020-09-24 16:22:15 -0600

    Added articles listing page

---
+++ app/models/article.rb
@@ -0,0 +1,7 @@
+#your article class must inherit from the application record
+class Article < ApplicationRecord
+  #validation/constraint for article creation
+  #This makes sure that a title, description, and lengths of each is present in order for an article to be saved in the database
+  validates :title, presence: true, length: { minimum: 6, maximum: 100 }
+  validates :description, presence: true, length: { minimum: 10, maximum: 300 }
+end
