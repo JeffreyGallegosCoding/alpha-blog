4ac9bf2a515a45056d5dd42a6e5d0bb43fa26366
JeffreyGallegosCoding <jeffreygallegos01@gmail.com>
2020-10-01 13:01:08 -0600

    Added users creation action and updated nav temporarily

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,20 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    #Similar to articles create method in articles controller
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "Welcome to the Alpha Blog, #{@user.username} you're all signed up!"
+      redirect_to articles_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 end
