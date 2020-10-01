5757753c3e99daf999c319e29dc14c1958bbf1b7
JeffreyGallegosCoding <jeffreygallegos01@gmail.com>
2020-10-01 12:17:25 -0600

    Created new user base sign up form

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,10 @@ Rails.application.routes.draw do
   #resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy]
   #Since the above lists out every route that you can perform you can write this code as:
   resources :articles
+  #For users to see and sign up on the form
+  get 'signup', to: 'users#new'
+  #You can use the post method below for the users to actually submit the form
+  #post 'users', to: 'users#create'
+  #Or you can use the resources method below to get all of the routes except new
+  resources :users, except: [:new]
 end
