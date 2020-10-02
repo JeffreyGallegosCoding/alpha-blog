5498c7b7ab726343ccfc476c57acf960389fefb3
JeffreyGallegosCoding <jeffreygallegos01@gmail.com>
2020-10-02 10:44:33 -0600

    Added authentication helper methods, updated nav, cleaned up duplicate code in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,11 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
+
   def new
     @user = User.new
   end
 
   def show
-    @user = User.find(params[:id])
     #Gives articles instance variable and pulls all articles associated with user into the variable
     @articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
@@ -17,6 +18,7 @@ class UsersController < ApplicationController
     #Similar to articles create method in articles controller
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "Welcome to the Alpha Blog, #{@user.username} you're all signed up!"
       redirect_to articles_path
     else
@@ -26,11 +28,9 @@ class UsersController < ApplicationController
 
   def edit
     #This will find the user via their id on the users table
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "Your account info was updated successfully!"
       redirect_to @user
@@ -43,5 +43,9 @@ class UsersController < ApplicationController
   private
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
