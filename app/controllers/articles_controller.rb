fd6f861485699e6fcee87c6ec9fb2f2ce74d9c90
JeffreyGallegosCoding <jeffreygallegos01@gmail.com>
2020-10-02 11:54:58 -0600

    Added restrictions for current users so they cant edit/delete other users work or account info

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -30,7 +30,7 @@ class ArticlesController < ApplicationController
 
     #Need to include so it will be whitelisted
     @article = Article.new(article_params)
-    @article.user = User.first
+    @article.user = current_user
     #Then you save the article
     if @article.save
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,19 +17,4 @@ module ApplicationHelper
     image_tag(gravatar_url, class: "rounded shadow mx-auto d-block", alt: user.username)
   end
 
-  # Returns the details of the user that is logged in
-  def current_user
-    # Return current user if you have cu already if not then do a query to find the user
-    # This is helpful so that everytime the current_user method is called, the application
-    # is not making a call to the user's table for the user info. Instead it just uses the
-    # instance variable.
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  end
-
-  # Check if there is user that is already logged in
-  def logged_in?
-    # Check if the current user exists by turning into a boolean (either true/false)
-    !!current_user
-  end
-
 end
