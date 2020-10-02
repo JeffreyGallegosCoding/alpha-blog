eb078bb601108a78dd80b8f77fb372b5ee4b0d49
JeffreyGallegosCoding <jeffreygallegos01@gmail.com>
2020-10-02 10:11:02 -0600

    Added user login and logout functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,11 +5,26 @@ class SessionsController < ApplicationController
   end
 
   def create
-    
+    # Find the user by their email from their session params
+    user = User.find_by(email: params[:session][:email].downcase)
+    # If the user is found
+    if user && user.authenticate(params[:session][:password])
+      # Creates a user session according the user id and keeps them in a logged in state
+      session[:user_id] = user.id
+      flash[:notice] = "Logged in successfully"
+      redirect_to user
+      # If the user is not found
+    else
+      # Use flash now because you are not being redirected to a different page but staying on the same form
+      flash.now[:alert] = "There was somthing wrong with your log in details"
+      render 'new'
+    end
   end
 
   def destroy
-
+    session[:user_id] = nil
+    flash[:notice] = "Later"
+    redirect_to root_path
   end
 
 end
