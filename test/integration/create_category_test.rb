8bf4a10317d541b20946a52067191b9e41172c69
JeffreyGallegosCoding <jeffreygallegos01@gmail.com>
2020-10-14 16:55:26 -0600

    Added an example of an invalid category creation integration test

--- test/integration/create_category_test.rb
+++ test/integration/create_category_test.rb
@@ -15,4 +15,21 @@ class CreateCategoryTest < ActionDispatch::IntegrationTest
     # See if redirect takes you to the correct show page. Finding it by the name in the html body.
     assert_match "Sports", response.body
   end
+
+  test "get new category form reject invalid category submission" do
+    get "/categories/new"
+    assert_response :success
+    # If there is an invalid submission then there should be no difference in the category count
+    assert_no_difference 'Category.count' do
+      post categories_path, params: { category: { name: " " } }
+    end
+    # See if redirect takes you to the error messages. You test to see if the assert matches text
+    # in the response html body but you use assert_select to search through elements on the page
+    # in the response such as the div class of alert and the h4 class of alert-heading
+    # Warning!!: If you are trying to match with an html element it can also match with any notes you have in your code
+    assert_match "following", response.body
+    assert_select 'div.alert'
+    assert_select 'h4.alert-heading'
+  end
+
 end
