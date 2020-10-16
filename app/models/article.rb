7c85ce74a3f89f9966885fb2584ff5bce8d99ec3
JeffreyGallegosCoding <jeffreygallegos01@gmail.com>
2020-10-16 10:15:57 -0600

    Added articlecategory table, model, and many to many associations between articles and categories

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,9 +1,13 @@
-#your article class must inherit from the application record
+# your article class must inherit from the application record
 class Article < ApplicationRecord
-  #an article can only belong to one user
+  # an article can only belong to one user
   belongs_to :user
-  #validation/constraint for article creation
-  #This makes sure that a title, description, and lengths of each is present in order for an article to be saved in the database
+  # define the many to many relationship between articles and categories
+  # This code can be seen on rails guides
+  has_many :article_categories
+  has_many :categories, through: :article_categories
+  # validation/constraint for article creation
+  # This makes sure that a title, description, and lengths of each is present in order for an article to be saved in the database
   validates :title, presence: true, length: { minimum: 6, maximum: 100 }
   validates :description, presence: true, length: { minimum: 10, maximum: 300 }
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,7 @@
 class Category < ApplicationRecord
   validates :name, presence: true, length: { minimum: 3, maximum: 25 }
   validates_uniqueness_of :name
+  # defines many to many relationship between categories and articles
+  has_many :article_categories
+  has_many :articles, through: :article_categories
 end
